2a6671ec1f3770a7df99f239437780133c5cfb65
Icaro Carvalho <icarocmessias@gmail.com>
2025-01-16 09:29:01 -0300

    PhotoPerfil

--- Familia/src/services/FML.Familiares.API/Services/Interface/IRelativeService.cs
+++ Familia/src/services/FML.Familiares.API/Services/Interface/IRelativeService.cs
@@ -5,9 +5,7 @@ namespace FML.Familiares.API.Services.Interface
     public interface IRelativeService
     {
         Task<IEnumerable<Relative>> GetRelatives();
-        Task<Guid> Add();
-        Task<bool> Update(UpdateRelativeModel relative, CancellationToken cancellationToken);
-        Task<bool> AddRelative(Relative relative);
+        Task<Guid> CargaInicial();
         Task<bool> RemoveRelative(Guid relativeId);
         Task<Relative> GetRelativeById(Guid relativeId);
     }
